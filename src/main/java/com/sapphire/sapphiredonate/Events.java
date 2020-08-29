6339708cb94dcddaba02a33659f4dfb8704ccbdf
JSONCMD <develnet.info@gmail.com>
2020-08-29 16:02:13 +0300

    Откат до стандартного способа

--- src/main/java/com/sapphire/sapphiredonate/Events.java
+++ src/main/java/com/sapphire/sapphiredonate/Events.java
@@ -3,7 +3,6 @@ package com.sapphire.sapphiredonate;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.configuration.file.YamlConfiguration;
-import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -16,49 +15,60 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
-import ru.sapphirelife.*;
-
 public class Events implements Listener {
 
     private final JavaPlugin core;
-    private List<ItemStack> ids = new ArrayList<>();
+    private List<String> ids = new ArrayList<>();
     private final String prefix = ChatColor.AQUA + "" + ChatColor.BOLD + "" + "Донат " + ChatColor.GOLD + "| " + ChatColor.RESET;
 
+    // ==============================================
+    // паки
+    String ECONOMY_PACK = "SAPPHIREDONATE_ECONOMY";
+    String MAGIC_PACK = "SAPPHIREDONATE_MAGIC";
+    String TECHNO_PACK = "SAPPHIREDONATE_TECHNO";
+    String ADVANCED_MAGIC_PACK = "SAPPHIREDONATE_ADVANCED";
+
+    // свитки возврата
+    String SCROLL_NEARNESS_10 = "SAPPHIREDONATE_SCROLL_10";
+    String SCROLL_NEARNESS_25 = "SAPPHIREDONATE_SCROLL_25";
+    String SCROLL_NEARNESS_50 = "SAPPHIREDONATE_SCROLL_50";
+    // ==============================================
+
     public Events(JavaPlugin core) {
-        SapphireMain main = SapphireMain.getInstance();
         this.core = core;
 
         // добавляем все id в лист
-        ids.add(main.getEconomyPack());
-        ids.add(main.getMagicPack());
-        ids.add(main.getTechnoPack());
-        ids.add(main.getAdvancedMagicPack());
+        ids.add(ECONOMY_PACK);
+        ids.add(MAGIC_PACK);
+        ids.add(TECHNO_PACK);
+        ids.add(ADVANCED_MAGIC_PACK);
 
-        ids.add(main.getScrollNearnessPack10());
-        ids.add(main.getScrollNearnessPack25());
-        ids.add(main.getScrollNearnessPack50());
+        ids.add(SCROLL_NEARNESS_10);
+        ids.add(SCROLL_NEARNESS_25);
+        ids.add(SCROLL_NEARNESS_50);
     }
+
     @EventHandler
     private void click(PlayerInteractEvent e) {
         if (e.getAction().equals(Action.RIGHT_CLICK_AIR)) {
             Player p = e.getPlayer();
-            ItemStack current = p.getInventory().getItemInMainHand();
+            String current = p.getInventory().getItemInMainHand().getType().toString();
+            p.sendMessage(current);
             if (ids.contains(current)) {
                 final ItemStack itemStack = p.getInventory().getItemInMainHand();
                 e.getPlayer().getInventory().setItemInMainHand(new ItemStack(Material.AIR));
-                execute(p, current.getType().toString(), itemStack);
+                execute(p, current, itemStack);
             }
         }
     }
 
     private void execute(Player p, String way, ItemStack itemStack) {
-        File File = null;
         p.sendMessage(prefix + "Обработка...");
 
         // получение списка предметов
         List<ItemStack> items = new ArrayList<>();
-        File = new File(core.getDataFolder() + File.separator + way + ".yml");
-        YamlConfiguration cfg = YamlConfiguration.loadConfiguration(File);
+        File file = new File(core.getDataFolder() + File.separator + way + ".yml");
+        YamlConfiguration cfg = YamlConfiguration.loadConfiguration(file);
         int i = 0;
         while (cfg.getItemStack(String.valueOf(i)) != null) {
             items.add(cfg.getItemStack(String.valueOf(i)));
